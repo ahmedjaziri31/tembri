23c0ba376d2a61bc9fb1a13675032e2a0621f8b5
Ahmed jaziri <76073128+ahmedjaziri31@users.noreply.github.com>
2025-10-04 16:53:08 +0100

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1161,13 +1161,13 @@ export default function HomePage() {
                   {/* Social Media Icons */}
                   <div className="flex justify-center space-x-4 sm:space-x-6">
                     <a 
-                      href="https://www.facebook.com/maisonelaris" 
+                      href="https://x.com/maisonelaris" 
                       target="_blank" 
                       rel="noopener noreferrer"
                       className="w-10 h-10 sm:w-12 sm:h-12 bg-[#336b62] rounded-full flex items-center justify-center hover:bg-[#9b8075] transition-colors duration-300"
                     >
                       <svg className="w-5 h-5 sm:w-6 sm:h-6 text-white" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
                       </svg>
                     </a>
                     <a 
